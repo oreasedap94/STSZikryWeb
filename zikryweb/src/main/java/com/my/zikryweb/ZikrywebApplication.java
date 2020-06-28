4ba8185094dd92d5c489452665923d8a1c2057c1
Admin <Admin@LAPTOP-B3JNSOHC>
2020-06-28 14:39:21 +0800

    First Commit

---
+++ zikryweb/src/main/java/com/my/zikryweb/ZikrywebApplication.java
@@ -0,0 +1,13 @@
+package com.my.zikryweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZikrywebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZikrywebApplication.class, args);
+	}
+
+}
---
+++ zikryweb/src/test/java/com/my/zikryweb/ZikrywebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.zikryweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZikrywebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
